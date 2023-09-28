f7434ad614d24a270571f770fff62a91720a2193
Chris Graveline <chris.graveline@showcaseidx.com>
2023-09-27 23:00:49 -0400

    feat: updates to cards and homepage

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 ## The Coffee Seeker
 
-Leading a 🐎 to ☕️.
-
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,5 @@
 import { FC, PropsWithChildren } from 'react'
-import style from './Card.module.scss'
+import style from '../styles/Card.module.scss'
 
 export const Card: FC<PropsWithChildren> = ({ children }) => {
   return <div className={style.card}>{children}</div>
